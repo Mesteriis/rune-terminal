0213bebf251395b3d03d87a4e4e2446c8fa25df3
Aleksandr Meshchriakov <avm@sh-inc.ru>
2026-04-18 17:53:41 +0200

    refactor(ui): split telemetryrequired into local widget contract files

---
+++ frontend/ui/widgets/RTAIPanelWidget/telemetryrequired.story.tsx
@@ -0,0 +1,12 @@
+import { TelemetryRequiredMessage } from "./telemetryrequired";
+
+function TelemetryRequiredMessageStory() {
+    return (
+        <div style={{ height: 700, backgroundColor: "#111827", color: "#ffffff" }}>
+            <TelemetryRequiredMessage />
+        </div>
+    );
+}
+
+export default TelemetryRequiredMessageStory;
+
