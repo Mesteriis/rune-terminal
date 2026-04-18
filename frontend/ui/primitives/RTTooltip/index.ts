493a0b84d39fd7f12f95db8e99a5babe1e83c935
Aleksandr Meshchriakov <avm@sh-inc.ru>
2026-04-18 14:12:18 +0200

    refactor(ui): split RTTooltip into style template logic story

--- frontend/ui/primitives/RTTooltip/index.ts
+++ frontend/ui/primitives/RTTooltip/index.ts
@@ -1,3 +1,4 @@
 // Copyright 2025, Command Line Inc.
 // SPDX-License-Identifier: Apache-2.0
-export * from "./RTTooltip";
+export * from "./RTTooltip.template";
+export type { TooltipProps } from "./RTTooltip.logic";
